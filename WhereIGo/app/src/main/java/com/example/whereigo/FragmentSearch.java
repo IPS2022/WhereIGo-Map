01f83f725549958d1361662eb071dbe0494a849c
Jinyoung Kim <khappy9888@sookmyung.ac.kr>
2022-05-12 23:27:03 +0900

    [feat] kakao map은 전체 위치 지도 맵 띄우는거까지 됨.
    구글로 다시 해봐야겠다 ㅠ

--- WhereIGo/app/src/main/java/com/example/whereigo/FragmentSearch.java
+++ WhereIGo/app/src/main/java/com/example/whereigo/FragmentSearch.java
@@ -7,11 +7,21 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
-    public class FragmentSearch extends Fragment {
+import net.daum.mf.map.api.MapView;
+
+public class FragmentSearch extends Fragment {
+    MapView mapView;
+
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_search, container, false);
+
+
+            //map api 화면 띄우기 코드
+            mapView=new MapView(getActivity());
+            ViewGroup mapViewContainer=rootView.findViewById(R.id.map_view);
+            mapViewContainer.addView(mapView);
 
             return rootView;
         }
